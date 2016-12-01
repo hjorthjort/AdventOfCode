9306daf8b7f0230939e19ef3342c762846cdd9f3
hjorthjort <rikard.hjort@gmail.com>
2016-12-01 17:23:28 +0100

    Final solution to taxi

--- Taxi.hs
+++ Taxi.hs
@@ -1,18 +1,41 @@
 import Data.List.Split(splitOn)
 
-main :: IO String
+main :: IO ()
 main = do
     input <- readFile "taxi.txt"
 
-    return $ show $ solve $ filter (/='\n') input
+    print $ solve $ filter (/='\n') input
 
-solve :: String -> Int
-solve s = undefined
-
-data Direction = L | R deriving (Show, Eq)
-
-parse :: String -> [(Direction, Int)]
+parse :: String -> [(Char, Int)]
 parse = map toDir . splitOn ", "
     where
-        toDir ('R':i) = (R, read i)
-        toDir ('L':i) = (L, read i)
+        toDir (h:i) = (h, read i)
+
+data Direction = North | East | South | West
+    deriving (Show, Eq)
+
+succ' West = North
+succ' North = East
+succ' East = South
+succ' South = West
+pred' North = West
+pred' East = North
+pred' South = East
+pred' West = South
+
+val dir (x,y) n
+    | dir == North = (x, y+n)
+    | dir == East = (x+n, y)
+    | dir == South = (x, y-n)
+    | dir == West = (x-n, y)
+
+solve :: String -> Int
+solve = solve' (0,0) North . parse
+
+solve' (x,y) _ [] = abs x + abs y
+solve' pos dir (a:as) = solve' pos' dir' as
+    where
+        lr = fst a
+        n = snd a
+        dir' = if lr == 'R' then succ' dir else pred' dir
+        pos' = val dir' pos n
