ebc83c4def4e03bfbf06ac70d3b79a7b63894b19
hjorthjort <rikard.hjort@gmail.com>
2016-12-04 02:05:52 +0100

    Solve part 2

--- 3/triangles.hs
+++ 3/triangles.hs
@@ -1,11 +1,15 @@
 import Data.List
+import Data.List.Split
 
 main = do
     input <- readFile "input.txt"
-    let triangles = map ( sort . (map read ) . words) $ lines input
-        printable = [x !! 0 + x !! 1 + x !! 2 | x <- triangles]
+    let triangles = map ( (map read ) . words) $ lines input
+        triangles1 = map sort triangles
+        triangles2 = map sort $ chunksOf 3 $ concat $ transpose triangles
+        printable = [x !! 0 + x !! 1 + x !! 2 | x <- triangles1]
 
-    print $ length $  possibles triangles
+    print $ length $ possibles triangles1
+    print $ length $ possibles triangles2
 
 possibles :: [[Integer]] -> [[Integer]]
 possibles = filter (\(a:b:c:[]) -> abs a + abs b > abs c)
