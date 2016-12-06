5c4e8e8b75071afa9146e6b6930129ba3c21e584
hjorthjort <rikard.hjort@gmail.com>
2016-12-06 12:42:11 +0100

    Solve problem 2

--- 6/repetition.hs
+++ 6/repetition.hs
@@ -3,8 +3,13 @@ import Data.List
 main = do
     input <- readFile "input.txt"
     print $ solvePart1 input
+    print $ solvePart2 input
 
 solvePart1 = map mostCommonLetter . transpose . lines
+solvePart2 = map leastCommonLetter . transpose . lines
 
-mostCommonLetter = head . head . reverse . sortOn length . group . sort
+mostCommonLetter = head . head . reverse . groupAndSort
+leastCommonLetter = head . head . groupAndSort
+
+groupAndSort = sortOn length . group . sort
 
