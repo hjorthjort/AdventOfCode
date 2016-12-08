8cf12e7aecf31a30298eeb6cf6bde9979e756e6c
hjorthjort <rikard.hjort@gmail.com>
2016-12-09 00:12:11 +0100

    Solve part 1

--- 7/ipv7.hs
+++ 7/ipv7.hs
@@ -1,16 +1,20 @@
 import Data.Char
+import Data.List
 import Data.List.Split
 
--- main = do
---     input <- "input.txt"
---     print $ parseId $ head input
+main = do
+    input <- readFile "input.txt"
+    print $ solve1 input
 
+solve1 :: String -> Int
 solve1 = length . filter supportsTLS . lines
 
 type HypSeq = String
 type Id = String
 
-supportsTLS = undefined
+supportsTLS = supportsTLS' . parse
+
+supportsTLS' (ids, hs) = any hasPalin4s ids && (not $ any hasPalin4s hs)
 
 -- parse :: String -> ([Id], [HypSeq])
 parse = (\l -> (evens l, odds l)) . splitOneOf ['[',']']
@@ -21,3 +25,11 @@ evens _ = []
 
 odds [] = []
 odds l = evens $ tail l
+
+groups size l = take (length l - size + 1) $ map (take size) $ tails l
+
+hasPalin4s :: String -> Bool
+hasPalin4s = any isPalin4 . fours
+    where
+        isPalin4 (a:b:c:d:[]) = a == d && b == c && a /= b
+        fours = groups 4
