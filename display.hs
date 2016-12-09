94798bddab65c2b6f2ac030a86b77d83cd011927
hjorthjort <rikard.hjort@gmail.com>
2016-12-09 02:31:14 +0100

    Parse better

--- display.hs
+++ display.hs
@@ -1,11 +1,10 @@
 import Data.Char
 
-
 parse = parse' . words
 
-parse' ("rect":[x]) = parseRect x
-parse' ("rotate":"row ":rest) = undefined
-parse' ("rotate":"column ":rest) = undefined
+parse' ("rect":[x]) = (rect, parseRect x)
+parse' ("rotate":"row":rest) = (row, parseRC rest)
+parse' ("rotate":"column":rest) = (col, parseRC rest)
  
 parseRect :: String -> (Int, Int)
 parseRect s = (read first, read second)
@@ -13,3 +12,8 @@ parseRect s = (read first, read second)
         first = takeWhile isDigit s
         second = takeWhile isDigit $ tail $ dropWhile isDigit s
 
+parseRC (('y':'=':row):_:rot:[]) = (read row, read rot)
+
+rect = 0
+row = 1
+col = 2
